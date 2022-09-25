599cf3a73715f29015c50102b04e637bddde6d9e
Alisha Saxena <alishasaxena2016@gmail.com>
2022-09-25 18:47:59 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const handleTask = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const addTask = (e) => {
+    e.preventDefault();
+    setTodoList((prevItem) => {
+      return [...prevItem, todo];
+    });
+    setTodo("");
+  };
+
+  const deleteItem = (index) => () => {
+    setTodoList((items) => items.filter((_, i) => i !== index));
+  };
+
+  const newItem = todoList.map((item, i) => {
+    return (
+      <div className="flex item" key={item[i]}>
+        <h4>{item}</h4>
+        <button className="trash" onClick={deleteItem(i)}>
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </div>
+    );
+  });
+
+  const removeAllTask = () => {
+    setTodoList([]);
+  };
+  console.log(newItem);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>TODOLIST</h1>
+      <form className="flex">
+        <input value={todo} type="text" onChange={handleTask}></input>
+        <button className="add-button" onClick={addTask}>
+          <i className="fa-solid fa-plus icon"></i> Add Task
+        </button>
+      </form>
+      {newItem}
+      {todoList.length > 0 ? (
+        <button onClick={removeAllTask} className="clear-button">
+          Clear All<i className="fa-solid fa-trash icon"></i>
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
-export default App;
